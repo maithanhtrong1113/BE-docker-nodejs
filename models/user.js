1cdd945de1ce44e8cde82150187c8e3551045b70
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2022-12-26 14:55:22 +0700

    first commit

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+  },
+  token: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
